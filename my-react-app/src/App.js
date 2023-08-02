475b2e066e3561d1c8dc918ba649c2f15711d0f8
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-08-02 23:06:50 +0200

    Add column functionality

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <>Hello</>
+    <div style={{  display: 'flex', alignItems: 'center',
+      justifyContent: 'center'}}>
+      <div class="col">
+        <p>Hello1</p>
+        <p>Hello2</p>
+        <p>Hello3</p>
+        <p>Hello4</p>
+      </div>
+    </div>
   );
 }
 
