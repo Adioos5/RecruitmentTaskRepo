1fc5092a2783da79b871ff4cc1cb572a45e0a2f3
Adioos5 <adrian.ciesla.stud@pw.edu.pl>
2023-08-02 23:12:58 +0200

    Add data fetching from API functionality

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div style={{  display: 'flex', alignItems: 'center',
       justifyContent: 'center'}}>
       <div class="col">
-        <p>Hello1</p>
-        <p>Hello2</p>
-        <p>Hello3</p>
-        <p>Hello4</p>
+        {data.map((item) => (
+          <div key={item.id}>
+            {item.title} <br/>
+            {item.body} <br/><br/>
+          </div>
+        ))}
       </div>
     </div>
   );
